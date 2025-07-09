4973088d47df3e04cbce7f1bef1828798c436257
virattt <virat.dot@gmail.com>
2025-07-09 17:58:26 -0400

    Update theming

--- app/frontend/src/components/panels/right/component-actions.tsx
+++ app/frontend/src/components/panels/right/component-actions.tsx
@@ -4,7 +4,7 @@ interface ComponentActionsProps {
 
 export function ComponentActions({ }: ComponentActionsProps) {
   return (
-    <div className="p-2 flex justify-between flex-shrink-0 items-center border-b border-ramp-grey-700 mt-4">
+    <div className="p-2 flex justify-between flex-shrink-0 items-center border-b mt-4">
       <span className="text-primary text-sm font-medium ml-4">Components</span>
       {/* <div className="flex items-center gap-1">
         <Button
